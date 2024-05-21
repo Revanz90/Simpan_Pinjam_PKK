85748b1874b9e280d3575bcc1c28f52c784e3d8e
xandroaprilianto <alexandrosandro58@gmail.com>
2024-05-21 17:41:52 +0700

    Tambah role pada blade resume transaksi

--- resources/views/layouts/resume_transaksi_angsuran.blade.php
+++ resources/views/layouts/resume_transaksi_angsuran.blade.php
@@ -62,7 +62,7 @@
                                 <td>{{ $anggota->id_anggota }}</td>
                                 <td>{{ $anggota->nama_anggota }}</td>
                                 <td>
-                                    @if($user->hasRole('admin'))
+                                    @if($user->hasRole('admin')|$user->hasRole('ketua')|$user->hasRole('bendahara'))
                                         {{ $nominalPerAuthor[$anggota->id_user] ?? 0 }}
                                     @else
                                         {{ $totalNominal }}
