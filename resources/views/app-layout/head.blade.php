7899f9d4df5172db3de9b3fa6c343a2db9999495
xandroaprilianto <alexandrosandro58@gmail.com>
2024-06-01 15:49:36 +0700

    Perbaiki tampilan laporan angsuran dan download lapora angsuran

--- resources/views/app-layout/head.blade.php
+++ resources/views/app-layout/head.blade.php
@@ -25,4 +25,13 @@
     <link rel="stylesheet" href="{{ asset('AdminLTE/plugins') }}/daterangepicker/daterangepicker.css">
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('AdminLTE/plugins') }}/summernote/summernote-bs4.min.css">
+    
+    <!-- CSS DatePicker -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- JS DatePicker -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 </head>
--- resources/views/layouts/laporan_angsuran.blade.php
+++ resources/views/layouts/laporan_angsuran.blade.php
@@ -1,191 +1,234 @@
-<!doctype html>
-<html lang="en">
+@extends('main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+@section('title', 'Laporan Angsuran')
+@section('title2', 'Laporan Angsuran')
+@section('judul', 'Laporan Angsuran')
 
-    <title>Cetak Laporan Angsuran</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
+@section('page-js-files')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+@stop
 
-        .header {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 1rem;
-        }
+@section('content')
+    <section class="content">
+        <div id="xtest" style="font-size: 14px"></div>
+        <div class="callout callout-warning">
+            <i class="fas fa-info-circle"></i>
+            Halaman untuk melihat dan mencetak data transaksi Angsuran
+        </div>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
 
-        .header-text {
-            place-items: center;
-            text-align: center;
-            margin-top: 1.6rem;
-            margin-bottom: 1.6rem;
-            margin-left: 10px;
-        }
+        @if ($message = Session::get('error'))
+            <div class="alert alert-danger alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
 
-        .brand-image {
-            max-width: 150px;
-        }
+        <div class="card">
+            <!-- Navbar Content -->
+            <div class="card-header">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2 class="card-title font-weight-bold">FILTER ANGSURAN</h2>
+                    <div class="d-flex justify-content-center">
+                        <div class="input-group w-100 mr-1">
+                            <input type="text" class="form-control" name="date-range" id="date-range" placeholder="Pilih Tanggal">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                            </div>
+                        </div>
+                        <div class="btn-group d-flex align-items-center w-100 mr-1" role="group">
+                            <button id="download-laporan-pdf" class="btn btn-warning form-control" role="button">
+                                CETAK LAPORAN
+                                <span class="btn-group-text"><i class="fas fa-print"></i></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Navbar Content -->
 
-        .title {
-            text-align: center;
-            padding-top: 2rem;
-        }
+            <!-- Page Content -->
+            <div class="card-body">
+                <table id="resume-angsuran-table" class="table table-bordered table-striped mb-4">
+                    <thead>
+                        <tr>
+                            <th>Total Angsuran</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Rp{{ $totalNilaiAngsuran }}</td>
+                        </tr>
+                    </tbody>
+                </table>
 
-        .title-container {
-            text-align: center;
-        }
-
-        .content {
-            display: flex;
-            justify-content: left;
-        }
-
-        .student-info {
-            margin-right: 1rem;
-        }
-
-        .student-info p {
-            margin: 0;
-            line-height: 1.5;
-        }
-
-        .table-container {
-            padding-top: 2rem;
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th,
-        td {
-            border: 1px solid black;
-            text-align: center;
-            padding: 8px;
-            vertical-align: middle;
-        }
-
-        .signature {
-            display: flex;
-            justify-content: flex-end;
-            padding-top: 1.5rem;
-        }
-
-        .signature-content {
-            text-align: center;
-        }
-
-        .signature p {
-            margin: 0;
-        }
-
-        hr {
-            border: none;
-            border-top: 2px solid black;
-        }
-    </style>
-</head>
-
-<body>
-    <!-- Header Content -->
-    <header class="header">
-        <div class="d-flex">
-            <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('img/Logo-PKK.png'))) }}"
-                alt="Logo SMK" name="logo-smk-yapemda" class="brand-image img-circle elevation-3">
-            <div class="header-text">
-                <p><strong>Sistem Informasi Simpan Pinjam</strong></p>
-                <p><strong>Pemberdayaan Kesejahteraan Keluarga (PKK)</strong></p>
-                <p><strong>Kelurahan Kalitirto, Berbah, Sleman</strong></p>
+                <table id="detail-angsuran-table" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>ID Anggota</th>
+                            <th>Nama Anggota</th>
+                            <th>Nominal Angsuran</th>
+                            <th>Tanggal Angsuran</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php $no = 1; @endphp
+                        @foreach ($getAngsurans as $angsuran)
+                            <tr>
+                                <th>{{ $no++ }}</th>
+                                <th>{{ $angsuran->author_id }}</th>
+                                <td>{{ $angsuran->author_name }}</td>
+                                <td>{{ $angsuran->nominal_angsuran }}</td>
+                                <td>{{ \Carbon\Carbon::parse($angsuran->tanggal_transfer)->formatLocalized('%d %B %Y') ?? '' }}</td>
+                                <td>{{ $angsuran->status }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-    </header>
+    </section>
 
-    <div class="card-body">
-        <div class="col-md-6">
-            <form action="" method="GET">
-                <div class="flex input-group gap-4">
-                    <select class="form-select"name="month_filter">
-                        <option value="">Bulan</option>
-                        <option value="januari">Januari</option>
-                        <option value="februari">Februari</option>
-                        <option value="maret">Maret</option>
-                        <option value="april">April</option>
-                        <option value="mei">Mei</option>
-                        <option value="juni">Juni</option>
-                        <option value="juli">Juli</option>
-                        <option value="agustus">Agustus</option>
-                        <option value="september">September</option>
-                        <option value="oktober">Oktober</option>
-                        <option value="november" type="submit">November</option>
-                        <option value="desember">Desember</option>
-                    </select>
+    <script>
+        $(document).ready(function(){
+            // Initialize the date range picker
+            $('#date-range').daterangepicker({
+                autoUpdateInput: false,
+                locale: {
+                    cancelLabel: 'Clear'
+                }
+            });
 
-                    <select class="form-select"name="year_filter">
-                        <option value="">Tahun</option>
-                        <option value="2023">2023</option>
-                        <option value="2024">2024</option>
-                    </select>
+            // Event handler for when the date range is applied
+            $('#date-range').on('apply.daterangepicker', function(ev, picker) {
+                $(this).val(picker.startDate.format('DD-MM-YYYY') + ' - ' + picker.endDate.format('DD-MM-YYYY'));
+                fetchFilteredData();
+            });
 
-                    <button type="submit" class="btn btn-primary">Filter</button>
-                </div>
-                <a href="{{ route('export_angsuran') }}" class="btn btn-warning mt-3">Cetak Angsuran</a>
-            </form>
-        </div>
-    </div>
+            // Event handler for when the date range is cleared
+            $('#date-range').on('cancel.daterangepicker', function(ev, picker) {
+                $(this).val('');
+                fetchFilteredData();
+            });
 
-    <!-- Title -->
-    <div class="title">
-        <div class="title-container">
-            <p><strong>Laporan Angsuran Bulanan</strong></p>
-        </div>
-    </div>
+            function getFilteredData() {
+                var dateRange = $('#date-range').val().split(' - ');
+                var startDate = dateRange[0] ? moment(dateRange[0], 'DD-MM-YYYY').format('YYYY-MM-DD') : null;
+                var endDate = dateRange[1] ? moment(dateRange[1], 'DD-MM-YYYY').format('YYYY-MM-DD') : null;
 
-    <!-- Table Content -->
-    <div class="table-container">
-        <table id="examplePolos" class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>ID Anggota</th>
-                    <th>Nama Anggota</th>
-                    <th>Nominal Angsuran</th>
-                    <th>Tanggal Angsuran</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($datas as $installment => $data)
-                    <tr>
-                        <th>{{ $data->author_id }}</th>
-                        <td>{{ $data->author_name }}</td>
-                        <td>{{ $data->nominal_angsuran }}</td>
-                        <td>{{ $data->tanggal_transfer }}</td>
-                        <td>{{ $data->status }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <p><strong>Catatan : Jatuh tempo PEMBAYARAN paling lambat Tanggal 10 tiap bulan</strong></p>
-    </div>
+                // Prepare data object
+                var data = {};
 
-    <!-- Signature -->
-    <div class="signature mb-3">
-        <div class="signature-content">
-            <p><strong>Yogyakarta, 18 Februari 2024</strong></p>
-            <p><strong>Ketua PKK,</strong></p>
-            <br><br><br>
-            <p><strong>Eni Kusrini Amd.Kep</strong></p>
-        </div>
-    </div>
+                if (startDate) data.start_date = startDate;
+                if (endDate) data.end_date = endDate;
 
-</body>
+                return data;
+            }
 
-</html>
+            function fetchFilteredData(){
+                var data = getFilteredData();
+                // Send the data to the controller via AJAX
+                $.ajax({
+                    url: '/laporan_angsuran',
+                    method: 'GET',
+                    data: data,
+                    success: function(response) {
+                        // Handle the response here, such as updating the page with the new data
+                        console.log(response);
+
+                        // Clear existing content
+                        $('#detail-angsuran-table tbody').empty();
+                        $('#resume-angsuran-table tbody').empty();
+
+                        // Initialize a counter for row numbers
+                        var counter = 1;
+
+                        // Iterate over each object in the response array
+                        response.getAngsurans.forEach(function(getAngsurans) {
+                            // Extract relevant data from the object
+                            var authorid = getAngsurans.author_id ? getAngsurans.author_id : '';
+                            var authorname = getAngsurans.author_name ? getAngsurans.author_name : '';
+                            var nominalangsuran = getAngsurans.nominal_angsuran ? getAngsurans.nominal_angsuran : '';
+                            var tanggaltransfer = getAngsurans.tanggal_transfer ? moment(getAngsurans.tanggal_transfer).format('MMMM YYYY') : '';
+                            var status = getAngsurans.status ? getAngsurans.status.toUpperCase() : '';
+
+                            // Create a new table row with the extracted data
+                            var row = '<tr>' +
+                                    '<td>' + (counter || '') + '</td>' +
+                                    '<td>' + (authorid || '') + '</td>' +
+                                    '<td>' + (authorname || '') + '</td>' +
+                                    '<td>' + (nominalangsuran || '') + '</td>' +
+                                    '<td>' + (tanggaltransfer || '') + '</td>' +
+                                    '<td>' + (status || '') + '</td>' +
+                                '</tr>';
+
+                            // Append the new row to the table body
+                            $('#detail-angsuran-table tbody').append(row);
+
+                            // Increment the counter
+                            counter++;
+
+                            // Create a new table row with the extracted data
+                            var rowResume = '<tr>' +
+                                    '<td>' + (response.totalNilaiAngsuran || '') + '</td>'
+                                '</tr>';
+
+                            // Append the new row to the table body
+                            $('#resume-angsuran-table tbody').append(rowResume);
+                        });
+                    },
+                    error: function(xhr, status, error) {
+                        // Handle any errors here
+                        console.error(xhr.responseText);
+                    }
+                });
+            }
+
+            // Event handler for when the "CETAK KARTU SPP" button is clicked
+            $('#download-laporan-pdf').click(function(e) {
+                e.preventDefault(); // Prevent the default action of the link
+                
+                var data = getFilteredData();
+
+                // Generate the URL with the query parameters
+                var url = "{{ route('export_angsuran') }}?" + $.param(data);
+
+                // Redirect the user to the generated URL
+                window.location.href = url;
+            });
+        });
+    </script>
+
+    <style>
+        /* Override default styles for Bootstrap Datepicker */
+        .datepicker-dropdown.show {
+            background-color: #fff !important; /* Set background color to white */
+            border: 1px solid #ced4da !important; /* Add border */
+            color: #000 !important; /* Set text color to black */
+        }
+
+        /* Override text color for Bootstrap Datepicker input */
+        .datepicker-dropdown.show .datepicker-days .datepicker-switch,
+        .datepicker-dropdown.show .datepicker-days .table-condensed>thead>tr>th,
+        .datepicker-dropdown.show .datepicker-days .table-condensed>tbody>tr>td,
+        .datepicker-dropdown.show .datepicker-days .table-condensed>tbody>tr>th,
+        .datepicker-dropdown.show .datepicker-months .datepicker-switch,
+        .datepicker-dropdown.show .datepicker-months .table-condensed>thead>tr>th,
+        .datepicker-dropdown.show .datepicker-months .table-condensed>tbody>tr>td,
+        .datepicker-dropdown.show .datepicker-months .table-condensed>tbody>tr>th,
+        .datepicker-dropdown.show .datepicker-years .datepicker-switch,
+        .datepicker-dropdown.show .datepicker-years .table-condensed>thead>tr>th,
+        .datepicker-dropdown.show .datepicker-years .table-condensed>tbody>tr>td,
+        .datepicker-dropdown.show .datepicker-years .table-condensed>tbody>tr>th {
+            color: #000 !important; /* Set text color to black */
+        }
+    </style>
+@endsection
